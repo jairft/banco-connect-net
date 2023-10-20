93fe54382cf4df4a8a518f83e340c4ce58e1d037
jairfreitas <jfreitas@ibm.com>
2023-10-20 10:05:07 -0300

    projeto banco-connect-net iniciado

---
+++ cliente-bank-cadastro/src/test/java/com/banco/connect/net/clientebankcadastro/ClienteBankCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco.connect.net.clientebankcadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteBankCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
